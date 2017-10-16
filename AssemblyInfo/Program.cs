ed64a1affcdddce7de16474a0b57c28c022836a5
Mansoor Omrani <mansoor.omrani@gmail.com>
2017-10-16 08:43:09 +0330

    Removed 'ok' variable and the Exception in first catch block
    There was no need to 'ok' variable. Also, the Exception in the first catch block had not been used. So, I removed it too.

--- AssemblyInfo/Program.cs
+++ AssemblyInfo/Program.cs
@@ -10,7 +10,6 @@ namespace AssemblyInfo
             if (args != null && args.Length > 0)
             {
                 Assembly asm = null;
-                var ok = false;
                 var name = args[0];
 
                 try
@@ -19,43 +18,33 @@ namespace AssemblyInfo
                         name += ".dll";
 
                     asm = Assembly.LoadFrom(name);
-
-                    ok = true;
                 }
-                catch (Exception e1)
+                catch
                 {
                     try
                     {
                         var path = AppDomain.CurrentDomain.BaseDirectory + @"\" + name;
 
                         asm = Assembly.LoadFrom(path);
-                        ok = true;
                     }
-                    catch (Exception e2)
+                    catch (Exception e)
                     {
-                        System.Console.WriteLine(e2.Message);
+                        System.Console.WriteLine(e.Message);
 
                         return;
                     }
                 }
 
-                if (ok)
-                {
-                    var x = asm.GetName();
+                var x = asm.GetName();
 
-                    System.Console.WriteLine("CodeBase: {0}", x.CodeBase);
-                    System.Console.WriteLine("ContentType: {0}", x.ContentType);
-                    System.Console.WriteLine("CultureInfo: {0}", x.CultureInfo);
-                    System.Console.WriteLine("CultureName: {0}", x.CultureName);
-                    System.Console.WriteLine("FullName: {0}", x.FullName);
-                    System.Console.WriteLine("Name: {0}", x.Name);
-                    System.Console.WriteLine("Version: {0}", x.Version);
-                    System.Console.WriteLine("VersionCompatibility: {0}", x.VersionCompatibility);
-                }
-                else
-                {
-                    System.Console.WriteLine("not found");
-                }
+                System.Console.WriteLine("CodeBase: {0}", x.CodeBase);
+                System.Console.WriteLine("ContentType: {0}", x.ContentType);
+                System.Console.WriteLine("CultureInfo: {0}", x.CultureInfo);
+                System.Console.WriteLine("CultureName: {0}", x.CultureName);
+                System.Console.WriteLine("FullName: {0}", x.FullName);
+                System.Console.WriteLine("Name: {0}", x.Name);
+                System.Console.WriteLine("Version: {0}", x.Version);
+                System.Console.WriteLine("VersionCompatibility: {0}", x.VersionCompatibility);
             }
             else
             {
