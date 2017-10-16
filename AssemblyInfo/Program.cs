36e5619c27c9cc7ac9c0ddbe2b61eddb1a89abc7
Mansoor Omrani <mansoor.omrani@gmail.com>
2017-10-16 08:23:10 +0330

    Initial commit

---
+++ AssemblyInfo/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                Assembly asm = null;
+                var ok = false;
+                var name = args[0];
+
+                try
+                {
+                    if (name.Substring(name.Length - 4, 4) != ".dll")
+                        name += ".dll";
+
+                    asm = Assembly.LoadFrom(name);
+
+                    ok = true;
+                }
+                catch (Exception e1)
+                {
+                    try
+                    {
+                        var path = AppDomain.CurrentDomain.BaseDirectory + @"\" + name;
+
+                        asm = Assembly.LoadFrom(path);
+                        ok = true;
+                    }
+                    catch (Exception e2)
+                    {
+                        System.Console.WriteLine(e2.Message);
+
+                        return;
+                    }
+                }
+
+                if (ok)
+                {
+                    var x = asm.GetName();
+
+                    System.Console.WriteLine("CodeBase: {0}", x.CodeBase);
+                    System.Console.WriteLine("ContentType: {0}", x.ContentType);
+                    System.Console.WriteLine("CultureInfo: {0}", x.CultureInfo);
+                    System.Console.WriteLine("CultureName: {0}", x.CultureName);
+                    System.Console.WriteLine("FullName: {0}", x.FullName);
+                    System.Console.WriteLine("Name: {0}", x.Name);
+                    System.Console.WriteLine("Version: {0}", x.Version);
+                    System.Console.WriteLine("VersionCompatibility: {0}", x.VersionCompatibility);
+                }
+                else
+                {
+                    System.Console.WriteLine("not found");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Usage: asminfo.exe assembly");
+            }
+        }
+    }
+}
